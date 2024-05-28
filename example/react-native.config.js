85eaa9454f0192f2f2a5a368398cb5849c16b69c
mac <mac@macs-MacBook-Pro.local>
2024-05-28 16:05:04 +0530

    build(ios): fix ios build issue and pod install

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 const pak = require('../package.json');
 
 module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
   dependencies: {
     [pak.name]: {
       root: path.join(__dirname, '..'),
